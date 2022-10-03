4e7838cbb89cfa9f5f22b310be62202a0fdbffae
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-10-03 09:25:40 +0100

    Feature: se añadió el Header, se añadio el esqueleto del proyecto.
    El header contiene mi nombre y mi foto de perfil.
    
    El esqueleto contiene la estructura que a priori tendrá el proyecto.

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <View style = {styles.header}>
+        <Image style = {styles.headerImg} source={require("./assets/2D3AA337-3ABF-4A30-BE4B-BB7511555AEE2.png")}/>
+        <Text style = {styles.headerName}>Darío Chinea Delgado</Text>
+      </View>
+      <View style = {styles.introduccion}> 
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container : {
     justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
   },
+  header : {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'grey',
+    justifyContent: 'space-between'
+  },
+  introduccion : {
+    flex: 4
+  },
+  headerImg : {
+    width: 100, 
+    height: 100, 
+    borderRadius: 50,
+    marginLeft: 10
+  },
+  headerName : {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: 'white',
+    marginRight: 20
+  }
 });
